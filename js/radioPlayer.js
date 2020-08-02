c6c3b629e2fb760e1d26b03303aead6ed707f3a1
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-02 23:29:29 +0300

    [DS-001] Radio: add play&stop buttons

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,8 +1,15 @@
 export const radioPlayer = {
     init: () => {
+        const button = {
+            play: document.querySelector('#playButton'),
+            stop: document.querySelector('#stopButton')
+        };
+
         const audio = new Audio();
         audio.type = 'audio/acc';
         audio.src = 'http://online.radioroks.ua/RadioROKS';
-        audio.play();
+
+        button.play.addEventListener('click', () => audio.play());
+        button.stop.addEventListener('click', () => audio.pause());
     }
 };
